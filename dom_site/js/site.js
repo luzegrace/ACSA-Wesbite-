04c21226fa25f01675eb5d7ad0c28b312117436d
Christopher Rael <Christopher.Rael@gmail.com>
2021-06-04 00:20:28 -0700

    Make project tiles data-driven. Add project selection functionality.

--- dom_site/js/site.js
+++ dom_site/js/site.js
@@ -2,27 +2,50 @@ var siteModule = (function () {
 
     // public
     var init = function () {
-        $(window).load(function () {
-            $(".logoload").delay(500).fadeOut(1000);
-        });
-
-        $("divbg").each(function (i) {
-            this.style.background = getRandomBackgroundColor(i);
-        });
-
-        $("grid-item").on(
-            "site.rotateImg",
-            function (e, mouseMoveEvent) {
-                rotateOnMouse(mouseMoveEvent, $(this));
-            }
-        );
-
-        $(document).mousemove(function (e) {
-            $("grid-item").trigger("site.rotateImg", [ e ]);
-        });
+        fadeOutLogo();
+        buildProjectTiles();
+        toggleMouseResponse(true);
     };
 
     // private
+    var fadeOutLogo = function () {
+        $(window).load(function () {
+            $(".logoload").delay(500).fadeOut(1000);
+        });
+    };
+
+    var buildProjectTiles = function () {
+        let grid = $("#projectGrid");
+        dataModule.projects.forEach(function (project, i) {
+            var divbg = $("<divbg><grid-item><img src=\"https://www.freeiconspng.com/thumbs/white-arrow-png/white-arrow-transparent-png-21.png\" /></grid-item></divbg>");
+            divbg.appendTo(grid);
+            project.color = getRandomBackgroundColor(i);            
+            divbg.css("background-color", project.color);
+            var gridItemImage = divbg.find("grid-item img");
+            gridItemImage.attr("alt", project.title);
+            gridItemImage.attr("title", project.title);
+            gridItemImage.data("projectIndex", i);
+            project.ref = gridItemImage;
+        });
+
+        $(document).on(
+            "site.rotateImg",
+            "grid-item img",
+            function (e, targetX, targetY) {
+                rotateGridItemImage($(this), targetX, targetY);
+            }
+        );
+
+        $(document).on(
+            "click",
+            "grid-item img",
+            function (e) {
+                handleProjectSelection(e, $(this).data("projectIndex"));
+                return false;
+            }
+        );
+    };
+
     var getRandomValue = function (min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     };
@@ -35,23 +58,81 @@ var siteModule = (function () {
                   )`;
     };
 
-    var rotateOnMouse =  function (moveEvent, gridItem) {
+    var rotateGridItemImage = function (gridItemImage, targetX, targetY) {
+        let radians;
+        if (gridItemImage.hasClass("active")) {
+            return;
+        }
+
+        var gridItem = gridItemImage.parent();
         var offset = gridItem.offset();
         var centerX = (offset.left) + (gridItem.width() / 2);
         var centerY = (offset.top) + (gridItem.height() / 2);
-        var mouseX = moveEvent.pageX;
-        var mouseY = moveEvent.pageY;
-        var distX = mouseX - centerX;
-        var distY = mouseY - centerY;
-        var radians = Math.atan2(distX, distY);
+
+        if (gridItemImage.hasClass("disabled")) {
+            radians = Math.atan2(centerX - targetX, centerY - targetY);
+        }
+        else {
+            radians = Math.atan2(targetX - centerX, targetY - centerY);
+        }
+
         var degree = (-90 + (radians * (180 / Math.PI) * -1));
 
         // only rotate the image relative to the center of the grid, not the entire grid
-        gridItem.find("img")
+        gridItemImage
             .css('-moz-transform', 'rotate(' + degree + 'deg)')
             .css('-webkit-transform', 'rotate(' + degree + 'deg)')
             .css('-o-transform', 'rotate(' + degree + 'deg)')
             .css('-ms-transform', 'rotate(' + degree + 'deg)');
+    }
+
+    var doTagsIntersect = function (projectIndex1, projectIndex2) {
+        var tags1 = dataModule.projects[projectIndex1].tags;
+        var tags2 = dataModule.projects[projectIndex2].tags;
+        var intersection = tags1.filter(value => tags2.includes(value));
+        return intersection.length > 0;
+    }
+
+    var handleProjectSelection = function (clickEvent, index) {
+        if (index >= 0
+            && !dataModule.projects[index].ref.hasClass("active")) {
+            toggleMouseResponse(false);
+            dataModule.projects.forEach(function (project, i) {
+                if (i == index) {
+                    project.ref.addClass("active").removeClass("related disabled");
+                }
+                else if (doTagsIntersect(i, index)) {
+                    project.ref.addClass("related").removeClass("active disabled");
+                }
+                else {
+                    project.ref.addClass("disabled").removeClass("active related");
+                }
+            });
+
+            var gridItem = dataModule.projects[index].ref.parent();
+            var targetOffset = gridItem.offset();
+            var centerX = (targetOffset.left) + (gridItem.width() / 2);
+            var centerY = (targetOffset.top) + (gridItem.height() / 2);
+            $("grid-item img").trigger("site.rotateImg", [centerX, centerY]);
+        }
+        else {
+            $("grid-item img").removeClass("active related disabled");      
+            $("grid-item img").trigger("site.rotateImg", [clickEvent.pageX, clickEvent.pageY]);
+            toggleMouseResponse(true);
+        }
+    };
+
+    var toggleMouseResponse = function (enabled) {
+        if (enabled) {
+            $(document).on(
+                "mousemove",
+                function (e) {
+                    $("grid-item img").trigger("site.rotateImg", [e.pageX, e.pageY]);
+                });
+        }
+        else {
+            $(document).off("mousemove");
+        }
     }
 
     return {
